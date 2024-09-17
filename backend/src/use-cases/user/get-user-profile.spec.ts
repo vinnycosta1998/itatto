81e6ff0dcdcf0aa1b1e0bb9d44bc7d1d952ea14d
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-17 18:12:41 -0300

    test get-user-profile

---
+++ backend/src/use-cases/user/get-user-profile.spec.ts
@@ -0,0 +1,33 @@
+import { randomUUID } from 'node:crypto'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get user profile test', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get profile user', async () => {
+    const createdUser = await usersRepository.create({
+      id: randomUUID(),
+      name: 'John Doe',
+      gender: 'male',
+      email: 'johndoe@example.com',
+      password: '12345678',
+      cpf: '11255549980',
+      cep: '81130590'
+    })
+
+    const { user } = await sut.execute({
+      id: createdUser.id
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John Doe')
+  })
+})
