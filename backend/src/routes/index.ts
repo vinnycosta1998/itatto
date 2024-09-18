3d75b2070390bcbcc17d3e5e44cd902bf1eab47c
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-18 15:54:38 -0300

    create tattoo route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,9 +2,14 @@ import type { FastifyInstance } from 'fastify'
 import { register } from '../http/controllers/user/register'
 import { authenticate } from '../http/controllers/user/authenticate'
 import { getUserProfile } from '../http/controllers/user/get-profile'
+import { createTatto } from '../http/controllers/tattoo/create-tattoo'
 
 export async function appRoutes(app: FastifyInstance) {
+  // User routes
   app.post('/register', register)
   app.post('/authenticate', authenticate)
   app.get('/me', getUserProfile)
+
+  // Tatto routes
+  app.post('create-tattoo', createTatto)
 }
