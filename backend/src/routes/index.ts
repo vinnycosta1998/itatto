13e7404820155170302da4cfa4d79d90e23ab683
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-18 11:59:15 -0300

    create register route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,3 +1,7 @@
 import type { FastifyInstance } from 'fastify'
+import { app } from '../app'
+import { register } from '../http/controllers/register'
 
-export async function appRoutes(app: FastifyInstance) {}
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/register', register)
+}
