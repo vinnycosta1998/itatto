e85f87cd16c4d221a9a2fa44c5cbf8cb5ac2e685
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-18 16:58:30 -0300

    create SearchManyTattoo route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import { authenticate } from '../http/controllers/user/authenticate'
 import { getUserProfile } from '../http/controllers/user/get-profile'
 import { createTatto } from '../http/controllers/tattoo/create-tattoo'
 import { deleteTattoo } from '../http/controllers/tattoo/delete-tattoo'
+import { SearchManyTattoo } from '../http/controllers/tattoo/search-many-tattoo'
 
 export async function appRoutes(app: FastifyInstance) {
   // User routes
@@ -14,4 +15,5 @@ export async function appRoutes(app: FastifyInstance) {
   // Tatto routes
   app.post('/create-tattoo', createTatto)
   app.delete('/delete-tattoo', deleteTattoo)
+  app.get('/search-tattoo', SearchManyTattoo)
 }
