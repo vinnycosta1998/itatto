55aaf8940022966f7e375b0698ebd0fd1c0fddd8
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-18 09:55:01 -0300

    create make-authenticate-use-case factorie

---
+++ backend/src/factories/make-authneticate-use-case.ts
@@ -0,0 +1,10 @@
+import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '../use-cases/user/authenticate'
+
+export function makeAuthenticateUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+
+  const useCase = new AuthenticateUseCase(usersRepository)
+
+  return useCase
+}
--- backend/src/factories/make-register-use-case.ts
+++ backend/src/factories/make-register-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '../use-cases/user/register'
 
-export function makeRegistereUseCase() {
+export function makeRegisterUseCase() {
   const usersRepository = new PrismaUsersRepository()
 
   const useCase = new RegisterUseCase(usersRepository)
