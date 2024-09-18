fee830410030a829299f8efb285e203eeb055faf
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-18 15:40:47 -0300

    create get-user-profile controller

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from '../http/controllers/register'
 import { authenticate } from '../http/controllers/authenticate'
+import { getUserProfile } from '../http/controllers/get-profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/register', register)
   app.post('/authenticate', authenticate)
+  app.get('/me', getUserProfile)
 }
