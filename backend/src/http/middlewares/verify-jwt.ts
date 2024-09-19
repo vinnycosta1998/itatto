970d5b7354ec23b06897a0ddc9f87fe8c69107d5
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-19 18:59:31 -0300

    create middleware auth jwt

---
+++ backend/src/http/middlewares/verify-jwt.ts
@@ -0,0 +1,11 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function verifyJWT(req: FastifyRequest, res: FastifyReply){
+    try{
+        await req.jwtVerify()
+    }catch(err){
+        return res.status(401).send({
+            message: 'Unathorized'
+        })
+    }
+}
