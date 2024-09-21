a5589ba99c8ad5fd1492b8206d8d3f0f31d4d181
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-21 19:00:15 -0300

    add uncontrolled components in profile page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,8 +1,46 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const updateProfileBodySchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, "A senha deve conter no minimo 8 caractheres")
+      .max(14, "A senha deve conter no máximo 14 caractheres"),
+    password_confirmation: z
+      .string()
+      .min(8, "A senha deve conter no minimo 8 caractheres")
+      .max(14, "A senha deve conter no máximo 14 caractheres"),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: "As senhas precisam ser iguais",
+    path: ["password_confirmation"],
+  });
+
+type UpdateProfileData = z.infer<typeof updateProfileBodySchema>;
+
 export default function Profile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UpdateProfileData>({
+    resolver: zodResolver(updateProfileBodySchema),
+  });
+
+  function handleUpdateUser(data: UpdateProfileData) {
+    console.log(data);
+  }
   return (
     <div className="w-screen h-screen bg-black flex flex-col items-center justify-center">
-      <div className="w-[30rem] h-[30rem] rounded-lg">
-        <form className="w-full h-full flex flex-col items-center gap-4">
+      <div className="w-[30rem] h-[40rem] rounded-lg">
+        <form
+          className="w-full h-full flex flex-col items-center gap-4 text-zinc-400"
+          onSubmit={handleSubmit(handleUpdateUser)}
+        >
           <h1 className="text-4xl neon-text">Altere os seus dados </h1>
           <div className="w-[26rem] flex justify-start">
             <label className="font-bold text-white">Nome</label>
@@ -23,13 +61,17 @@ export default function Profile() {
             readOnly
           />
           <div className="w-[26rem] flex justify-start">
-            <label className="font-bold text-white">Senhas</label>
+            <label className="font-bold text-white">Senha</label>
           </div>
           <input
             type="text"
             className="w-[26rem] h-12 bg-zinc-900 px-2 rounded-md outline-none"
             placeholder="Digite a sua senha"
+            {...register("password")}
           />
+          {errors.password && (
+            <span className="text-red-400">{errors.password.message}</span>
+          )}
           <div className="w-[26rem] flex justify-start">
             <label className="font-bold text-white">Confirme a senha</label>
           </div>
@@ -37,7 +79,13 @@ export default function Profile() {
             type="text"
             className="w-[26rem] h-12 bg-zinc-900 px-2 rounded-md outline-none"
             placeholder="Confirme a sua senha"
+            {...register("password_confirmation")}
           />
+          {errors.password_confirmation && (
+            <span className="text-red-400">
+              {errors.password_confirmation.message}
+            </span>
+          )}
           <button className="w-[26rem] h-12 bg-zinc-900 font-bold text-white rounded-md">
             Alterar dados
           </button>
