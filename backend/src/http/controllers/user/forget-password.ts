060f557c826ba70ed2a6a00040921d2786bd30b8
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-23 15:44:55 -0300

    add send emails with resend

--- backend/src/http/controllers/user/forget-password.ts
+++ backend/src/http/controllers/user/forget-password.ts
@@ -2,6 +2,10 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { makeForgetPassword } from "../../../factories/user/make-forget-password";
 import { UserNotFoundError } from "../../../errors/user-not-found-error";
+import { Resend } from "resend";
+import { env } from "../../../env";
+
+const resend = new Resend(env.RESEND_API_KEY)
 
 export async function forgetPassword(req: FastifyRequest, res:FastifyReply){
     const forgetPasswordBodyScehma = z.object({
@@ -18,7 +22,18 @@ export async function forgetPassword(req: FastifyRequest, res:FastifyReply){
         })
 
         if(user){
-            
+            const {data, error } = await resend.emails.send({
+                from: 'Acme <onboarding@resend.dev>',
+                to: [user.user.email],
+                subject: 'Recovery your password',
+                html: '<a>Click here for recovery your password<a/>'
+            })
+
+            if(error){
+                return console.error({ error })
+            }
+
+            console.log(data)
         }
     }catch(err){
         if(err instanceof UserNotFoundError){
