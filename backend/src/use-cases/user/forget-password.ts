00249febf0519694a3ef34e3535bff21fd56bb02
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-23 12:27:45 -0300

    create forget-password use-case

---
+++ backend/src/use-cases/user/forget-password.ts
@@ -0,0 +1,27 @@
+import { User } from "@prisma/client"
+import { UsersRepository } from "../../repositories/users-repository"
+import { UserNotFoundError } from "../../errors/user-not-found-error"
+
+interface ForgetPasswordRequest{
+    email: string
+}
+
+interface ForgetPasswordResponse{
+    user: User
+}
+
+export class Forgetpassword{
+    constructor(private usersRepository: UsersRepository){}
+
+    async execute({email} : ForgetPasswordRequest): Promise<ForgetPasswordResponse>{
+        const user = await this.usersRepository.findByEmail(email)
+
+        if(!user){
+            throw new UserNotFoundError()
+        }
+
+        return{
+            user
+        }
+    }
+}
